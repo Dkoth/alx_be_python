af167b3367e3c180b228bf89aa847da3b8e72b2a
Dkoth <davidokoth857@gmail.com>
2024-09-14 06:45:51 -0700

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+#Assigning numbers to variables 
+number1 = 10
+number2 = 5
+#Perform addition, subtraction, and multiplication on these numbers.
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+#Print the results
+print("Addition of [number1] and [number2] is [result]")
+print("Subtraction of [number1] and [number2] is [result]")
+print("Multiplication of [number1] and [number2] is [result]")
