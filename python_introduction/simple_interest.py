80360ec7cdc6e4419878f507d1fd3c581fc44940
Dkoth <davidokoth857@gmail.com>
2024-09-14 07:27:02 -0700

    Simple Interest Calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 #Define variables
-principal = 1000 (representing $1000)
-rate = 0.05 (representing 5% annual interest rate)
-time = 3 (representing 3 years)
+principal = 1000
+rate = 0.05
+time = 3
 #Calculate the simple interest
 interest = principal * rate * time
 #Print the calculated interest
