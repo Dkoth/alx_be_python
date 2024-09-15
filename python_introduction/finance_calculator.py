0cb6ecfdd0e20543636f8373f0365aeca5525ec1
Dkoth <davidokoth857@gmail.com>
2024-09-15 11:22:31 -0700

    Personal Finance Calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
 # Financial Details
-income = int(input("Enter your monthly income:"))
-expenses = int(input("Enter your total monthly expenses:"))
+income = float(input("Enter your monthly income:"))
+expenses = float(input("Enter your total monthly expenses:"))
 rate = 0.05
 #Calculate Monthly Savings and Project Annual Savings
-monthly savings = monthly expenses -  monthly income
-Projected annual savings = monthly savings * 12 + (monthly savings * 12 * 0.05)
+monthly_savings = monthly expenses -  monthly income
+Projected_annual savings = monthly savings * 12 + (monthly savings * 12 * 0.05)
 #Output Results
-print("monthly savings")
-print("projected annual savings")
+print("monthly_savings")
+print("projected_annual_savings")
