42f6f239e427019fe9795bdcb84ef13c80d2c389
Dkoth <davidokoth857@gmail.com>
2024-09-22 04:09:40 -0700

    Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 #weather_advice.py
 #Prompt User for Weather Input:
 weather_input = input("What's the weather like today? (sunny/rainy/cold): ")
-#Provide Clothing Recommendations based on user input:
-if weather_input.lower() == "sunny":
+#Provide Clothing Recommendations:
+if weather_input == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_input.lower() == "rainy":
+elif weather_input == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_input.lower() == "cold":
+elif weather_input == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
