298a78718fe860d91c4bb07c76978a892b61f3d2
Dkoth <davidokoth857@gmail.com>
2024-09-22 03:43:00 -0700

    Multiplication Table Generator

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+#multiplication_table.py
+#Prompt User for a Number:
+number = float(input("Enter a number to see its multiplication table:")
+#Generate and Print the Multiplication Table 1 to 10:
+for i range(1, 10):
+    result = number * i
+    print("{number} * {i} = {result}")
