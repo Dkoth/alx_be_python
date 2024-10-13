c317045b864dce586d322915812d5811deec3769
Dkoth <davidokoth857@gmail.com>
2024-10-13 12:06:23 -0700

    Exploring Polymorphism and Method Overriding

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -17,7 +17,7 @@ class Rectangle(Shape):
 
     class Circle(Shape):
         def __init__(self, radius):
-            self.radius - radius
+            self.radius = radius
 
         def area(self):
             return math.pi * (self.radius ** 2)
